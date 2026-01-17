62b6d9dd9b49cfab8dae67205b3eef95999d7ae7
Deansie <dennis-andersen@outlook.com>
2026-01-17 10:54:05 +0100

    feat: implement Kubernetes manifest application with client-go integration for automated deployments

---
+++ cmd/apply_exec.go
@@ -0,0 +1,116 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+var (
+	applyFile       string
+	applyDir        string
+	applyNamespace  string
+	applyKubeconfig string
+	applyDryRun     bool
+)
+
+var applyExecCmd = &cobra.Command{
+	Use:   "exec",
+	Short: "Execute apply on manifests",
+	RunE:  runApply,
+}
+
+func init() {
+	applyExecCmd.Flags().StringVar(&applyFile, "file", "", "Path to YAML manifest file")
+	applyExecCmd.Flags().StringVar(&applyDir, "dir", "", "Directory containing YAML manifests")
+	applyExecCmd.Flags().StringVar(&applyNamespace, "namespace", "", "Namespace to apply to (overrides manifest)")
+	applyExecCmd.Flags().StringVar(&applyKubeconfig, "kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "Path to kubeconfig")
+	applyExecCmd.Flags().BoolVar(&applyDryRun, "dry-run", false, "Simulate apply without changes")
+
+	_ = applyExecCmd.MarkFlagRequired("file")
+}
+
+func runApply(cmd *cobra.Command, args []string) error {
+	config, err := clientcmd.BuildConfigFromFlags("", applyKubeconfig)
+	if err != nil {
+		return fmt.Errorf("failed to load kubeconfig: %w", err)
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("failed to create dynamic client: %w", err)
+	}
+
+	var files []string
+	if applyFile != "" {
+		files = append(files, applyFile)
+	} else if applyDir != "" {
+		err = filepath.Walk(applyDir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() || filepath.Ext(path) != ".yaml" {
+				return nil
+			}
+			files = append(files, path)
+			return nil
+		})
+		if err != nil {
+			return fmt.Errorf("failed to walk dir: %w", err)
+		}
+	} else {
+		return fmt.Errorf("must specify --file or --dir")
+	}
+
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		if err != nil {
+			return fmt.Errorf("failed to read %s: %w", file, err)
+		}
+
+		dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+		obj := &unstructured.Unstructured{}
+		_, gvk, err := dec.Decode(data, nil, obj)
+		if err != nil {
+			return fmt.Errorf("failed to decode %s: %w", file, err)
+		}
+
+		if applyNamespace != "" {
+			obj.SetNamespace(applyNamespace)
+		}
+
+		resourceName := strings.ToLower(gvk.Kind) + "s"
+		gvr := schema.GroupVersionResource{
+			Group:    gvk.Group,
+			Version:  gvk.Version,
+			Resource: resourceName,
+		}
+
+		resource := dynClient.Resource(gvr).Namespace(obj.GetNamespace())
+
+		if applyDryRun {
+			fmt.Printf("Dry-run: Would apply %s %s in %s\n", obj.GetKind(), obj.GetName(), obj.GetNamespace())
+			continue
+		}
+
+		_, err = resource.Apply(context.Background(), obj.GetName(), obj, metav1.ApplyOptions{FieldManager: "k8s-deploy"})
+		if err != nil {
+			return fmt.Errorf("failed to apply %s: %w", file, err)
+		}
+
+		fmt.Printf("Applied %s %s in %s\n", obj.GetKind(), obj.GetName(), obj.GetNamespace())
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/cloudflare/cloudflare-go v0.116.0
 	golang.org/x/term v0.38.0
 	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/apimachinery v0.28.15
+	k8s.io/client-go v0.28.15
 )
 
 require (
@@ -27,14 +29,23 @@ require (
 	github.com/clipperhouse/displaywidth v0.4.1 // indirect
 	github.com/clipperhouse/stringish v0.1.1 // indirect
 	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/mango v0.1.0 // indirect
 	github.com/muesli/mango-cobra v1.2.0 // indirect
@@ -45,8 +56,17 @@ require (
 	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/time v0.9.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/klog/v2 v2.100.1 // indirect
+	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 )
